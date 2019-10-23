3541b89f54430b9f233d3fc9adbc4ecec26e2166
Sumit Chaudhari <sumit.nitsurat@gmail.com>
2019-10-23 16:10:59 +0800

    feat: 🎸 transfer ownership method added
    added method to transfer ownership of token

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { Document, getData } from "@govtechsg/open-attestation";
+import { get } from "lodash";
 import { getLogger } from "./util/logger";
 import { getProvider } from "./util/provider";
-import { getOwnerOf } from "./util/token";
+import { getOwnerOf, transferTokenOwnership } from "./util/token";
 
 const { trace } = getLogger("TokenInfo:");
 
@@ -12,9 +13,7 @@ interface TokenInterface {
 }
 
 /**
- * This function says hello.
- * @param name Some name to say hello for.
- * @returns The hello.
+ * Class Token to interact with ERC721 contract.
  */
 class Token implements TokenInterface {
   document: Document;
@@ -26,12 +25,27 @@ class Token implements TokenInterface {
     this.web3Provider = _web3Provider || getProvider();
   }
 
-  ownerOf(_tokenId: string): any {
+  private getIssuers(): Object[] {
+    const data = getData(this.document);
+    return data.issuers || [];
+  }
+
+  public async ownerOf(): Promise<string> {
     try {
-      trace("find owner of:", _tokenId);
-      const data = getData(this.document);
-      const issuers = data.issuers || [];
-      return getOwnerOf(_tokenId, issuers);
+      const tokenId = `0x${get(this.document, "[0].signature.targetHash")}`;
+      trace("find owner of:", tokenId);
+      return await getOwnerOf(tokenId, this.getIssuers());
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  }
+
+  public async transferOnwership(_to: string): Promise<boolean> {
+    try {
+      const from: string = await this.ownerOf();
+      const tokenId = `0x${get(this.document, "[0].signature.targetHash")}`;
+      trace(`transfer token ${tokenId} from ${from} to ${_to}`);
+      return await transferTokenOwnership(from, _to, tokenId, this.getIssuers());
     } catch (e) {
       throw new Error(e.message);
     }
--- src/util/token.ts
+++ src/util/token.ts
@@ -1,13 +1,27 @@
 import { Issuer } from "../types";
 import { contractInstance } from "../common/smartContract/contractInstance";
 
+const getContractInstance = (issuers: Issuer[]) => {
+  return issuers
+    .map(issuer => (issuer.tokenRegistry ? contractInstance({ contractAddress: issuer.tokenRegistry }) : null))
+    .filter(instance => !instance);
+};
+
 export const getOwnerOf = (_tokenId: string, issuers: Issuer[]) => {
-  const contractInstances = issuers.map(issuer =>
-    issuer.tokenRegistry ? contractInstance({ contractAddress: issuer.tokenRegistry }) : null
-  );
+  const contractInstances = getContractInstance(issuers);
   const owner = contractInstances.map(instance => instance.ownerOf(_tokenId)).find(o => o);
   if (!owner) {
     throw new Error("Invalid Document");
   }
   return owner;
+};
+
+export const transferTokenOwnership = async (_from: string, _to: string, _tokenId: string, issuers: Issuer[]) => {
+  try {
+    const contractInstances = getContractInstance(issuers);
+    await Promise.all(contractInstances.map(instance => instance.transferFrom(_from, _to, _tokenId)));
+    return true;
+  } catch (e) {
+    throw new Error(e.message);
+  }
 };
